e47a73d4dd7a04f0c005a9322b00876fef54f565
Hamoydy Dia <hamoydydia@gmail.com>
2024-11-08 20:48:22 +0100

    Ajout de l'image de l'architecte et perfection du formaulaire

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 document.querySelector('form').addEventListener('submit', function(e) {
-    e.preventDefault();
+    e.preventDefault(); // Empêche le rechargement de la page
+
+    // Affiche un message de confirmation pour l'utilisateur
     alert('Merci pour votre message ! Nous vous répondrons bientôt.');
+
+    // Envoie le formulaire
+    e.target.submit();
 });
